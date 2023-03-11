b7c144c70aadd5ff83a1c835f0602d955c8a4d8a
nate hishon <nathan.hishon@gmail.com>
2023-03-11 13:05:33 -0500

    updates

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('inside-mware', function () {
-        return response()->json('Success', 200);
-    });
+
+    Route::get('users/{id}', [\App\Http\Controllers\API\UserController::class, 'show']);
+
+    Route::post('logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
 });
